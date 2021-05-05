b32e7ad6671b1b390e8272a76a919b23c34e58fe
Nathan Shelton <nathan.shelton@student.swic.edu>
2021-05-05 13:23:22 -0500

    Changed from append to innerText

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-const user = document.getElementById("user");
-const opp = document.getElementById("opp");
+const user = document.getElementById('user');
+const opp = document.getElementById('opp');
 
 const userLife = 20;
 const oppLife = 20;
 
-user.appendChild(userLife);
-opp.appendChild(oppLife);
+user.innerText = userLife;
+opp.innerText = oppLife;
