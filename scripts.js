417e869c8aacd0f833cfd5122629ca52568f3a52
Nathan Shelton <nathan.shelton@student.swic.edu>
2021-05-05 13:38:46 -0500

    Add userPlus button to add 1 to userLife

--- scripts.js
+++ scripts.js
@@ -4,5 +4,12 @@ const opp = document.getElementById('opp');
 const userLife = 20;
 const oppLife = 20;
 
+// Todo: Turn the inner text and buttons into a renderList
 user.innerText = userLife;
 opp.innerText = oppLife;
+
+document.getElementById('userPlus').addEventListener('click', event => {
+  event.preventDefault();
+
+  user.innerText = userLife + 1;
+});
