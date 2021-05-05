e028e4b6a4e6f1a7dd767a261ec2ced028d44f5b
Nathan Shelton <nathan.shelton@student.swic.edu>
2021-05-05 13:14:13 -0500

    Add life totals by append to players

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
+const user = document.getElementById("user");
+const opp = document.getElementById("opp");
 
+const userLife = 20;
+const oppLife = 20;
+
+user.appendChild(userLife);
+opp.appendChild(oppLife);
