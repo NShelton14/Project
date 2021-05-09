71262a70d092d9015e2d937c45dace4af221fc39
Nathan Shelton <nathan.shelton@student.swic.edu>
2021-05-08 22:50:14 -0500

    User plus 1 button adds 1 to life counter

--- scripts.js
+++ scripts.js
@@ -1,15 +1,16 @@
 const user = document.getElementById('user');
 const opp = document.getElementById('opp');
 
-const userLife = 20;
+let userLife = 20;
 const oppLife = 20;
 
 // Todo: Turn the inner text and buttons into a renderList
+
 user.innerText = userLife;
-opp.innerText = oppLife;
 
 document.getElementById('userPlus').addEventListener('click', event => {
   event.preventDefault();
 
-  user.innerText = userLife + 1;
+  userLife += 1;
+  user.innerText = userLife;
 });
