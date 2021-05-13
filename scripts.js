f9881b2c463b67f6f4d75bf4a8cc76fe5362421e
Nathan Shelton <nathan.shelton@student.swic.edu>
2021-05-12 20:43:56 -0500

    Added comments to user and opp

--- scripts.js
+++ scripts.js
@@ -4,9 +4,11 @@ const user = document.getElementById('user');
 const userDiv = document.getElementById('userDiv');
 const colors = ['red', 'green', 'black', 'white', 'blue'];
 
+// Opponent life starting amount
 let oppLife = 20;
 opp.innerText = oppLife;
 
+// Opponent button color selector
 document
   .getElementById('oppColor')
   .querySelectorAll('button')
@@ -14,7 +16,7 @@ document
     button.addEventListener('click', event => {
       event.preventDefault();
       const divColor = event.target.className;
-
+      // Selecting class name instead of using id
       colors
         .filter(color => color !== divColor)
         .forEach(color => {
@@ -25,6 +27,7 @@ document
     });
   });
 
+// Opponent adding/subtracting buttons
 document
   .getElementById('oppMath')
   .querySelectorAll('button')
@@ -34,6 +37,7 @@ document
 
       const buttonId = event.target.id;
 
+      // Using switch case by targeting button id
       switch (buttonId) {
         case 'oppPlus':
           oppLife += 1;
@@ -61,6 +65,7 @@ document
         }
         case 'oppMinusFive':
           if (oppLife > 0 && oppLife - 5 >= 0) {
+            // Making sure life won't go below 0 when calculated
             oppLife -= 5;
             opp.innerText = oppLife;
           } else if (oppLife > 0) {
@@ -70,6 +75,7 @@ document
           break;
         case 'oppMinusTen':
           if (oppLife > 0 && oppLife - 10 >= 0) {
+            // Making sure life won't go below 0 when calculated
             oppLife -= 10;
             opp.innerText = oppLife;
           } else if (oppLife > 0) {
@@ -80,6 +86,7 @@ document
         case 'oppMinusIn': {
           const oppMinusInput = document.getElementById('oppMinusInput').value;
           if (oppLife > 0 && oppLife - oppMinusInput >= 0) {
+            // Making sure life won't go below 0 when calculated
             oppLife -= oppMinusInput;
             opp.innerText = oppLife;
           } else if (oppLife > 0) {
@@ -94,9 +101,11 @@ document
     });
   });
 
+// Starting user life and assigning it to p tag
 let userLife = 20;
 user.innerText = userLife;
 
+// User color selector buttons
 document
   .getElementById('userColor')
   .querySelectorAll('button')
@@ -115,6 +124,7 @@ document
     });
   });
 
+// User add/subtract buttons
 document
   .getElementById('userMath')
   .querySelectorAll('button')
@@ -124,6 +134,7 @@ document
 
       const buttonId = event.target.id;
 
+      // Using switch case by targeting button id
       switch (buttonId) {
         case 'userPlus':
           userLife += 1;
@@ -151,6 +162,7 @@ document
         }
         case 'userMinusFive':
           if (userLife > 0 && userLife - 5 >= 0) {
+            // Making sure life won't go below 0 when calculated
             userLife -= 5;
             user.innerText = userLife;
           } else if (userLife > 0) {
@@ -160,6 +172,7 @@ document
           break;
         case 'userMinusTen':
           if (userLife > 0 && userLife - 10 >= 0) {
+            // Making sure life won't go below 0 when calculated
             userLife -= 10;
             user.innerText = userLife;
           } else if (userLife > 0) {
@@ -171,6 +184,7 @@ document
           const userMinusInput = document.getElementById('userMinusInput')
             .value;
           if (userLife > 0 && userLife - userMinusInput >= 0) {
+            // Making sure life won't go below 0 when calculated
             userLife -= userMinusInput;
             user.innerText = userLife;
           } else if (userLife > 0) {
